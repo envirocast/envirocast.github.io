bafeecdb263aa6f77eecd36371d0aa91a2acf7b6
envirocast <kevitest.g3@gmail.com>
2025-09-13 15:42:35 -0500

    Update page.tsx

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -29,7 +29,7 @@ export default function TeamPage() {
       name: "Kavin Elangovan",
       role: "Web Applications and Graphics Developer",
       image: "/kavin.png",
-      bio: "A data-driven student with the goal of using the powers of innovation, creativity, and technology to improve connection and engagement using interactive interfaces. Created this informative and engaging website for EnviroCast and developed user interfaces and graphics for ML models and applications.",
+      bio: "A data-driven student with the goal of using the powers of innovation, creativity, and technology to improve connection and engagement using interactive interfaces. Created the informative and engaging EnviroCast website and Enviro AI. Developed ML models and web applications with interactive interfaces.",
       expertise: [
         "Web Development", "Software Applications", "Graphic Design", "Artificial Intelligence", "Machine Learning", "Image Editing", "API Interfaces",
       ],
