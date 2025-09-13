c9eeb907ce23ff75fe84ebd3ca4d6007d95298d1
envirocast <kevitest.g3@gmail.com>
2025-09-13 14:17:40 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   // Disable Next.js image optimization (important for GitHub Pages / static export)
   images: {
     unoptimized: true,
@@ -51,4 +50,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
