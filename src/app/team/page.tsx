cf21851f7be52365341c9816be3e03349349112e
envirocast <kevitest.g3@gmail.com>
2025-09-13 14:05:49 -0500

    Update page.tsx

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -4,6 +4,14 @@ import React from 'react';
 import { motion } from 'framer-motion';
 import { Mail, Heart, Layers, Target, Users, Calendar, Star, Briefcase, BrainCircuit, Sparkles, Send, MapPin } from 'lucide-react';
 
+// import local images
+// import ArnavImg from "./pfp/arnav.png";
+import KavinImg from "./pfp/kavin.png";
+// import VirImg from "./pfp/vir.png";
+// import DivinImg from "./pfp/divin.png";
+// import AhaanImg from "./pfp/ahaan.png";
+// import SathyanImg from "./pfp/sathyan.png";
+
 export default function TeamPage() {
   const teamMembers = [
     {
@@ -27,7 +35,7 @@ export default function TeamPage() {
       id: 2,
       name: "Kavin Elangovan",
       role: "Web Applications and Graphics Developer",
-      image: "https://placehold.co/400x400/60a5fa/ffffff?text=KE&font=inter",
+      image: KavinImg,
       bio: "A data-driven student with the goal of using the powers of innovation, creativity, and technology to improve connection and engagement using interactive interfaces. Created an informative and engaging website for EnviroCast and developed user interfaces and graphics for ML models and applications.",
       expertise: [
         "Web Development", "Software Applications", "Graphic Design", "Artificial Intelligence", "Machine Learning", "Image Editing", "API Interfaces",
